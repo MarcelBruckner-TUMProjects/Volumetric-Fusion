918b3aec1d4ab8b9c85c0b1f37f2eff1410fe5fe
Brucknem <mbruckner94@gmail.com>
2019-10-18 10:08:46 +0200

    Removed all to readd with new gitignore

--- LearnOpenGL/VolumetricFusion/GLHelpers.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <glad/glad.h> 
-#include <GLFW/glfw3.h>
-#include <stdio.h>
-#include <iostream>
-
-void error_callback(int error, const char* description);
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-unsigned int create_shader_program(const char* vertexShaderSource, const char* fragmentShaderSource);
