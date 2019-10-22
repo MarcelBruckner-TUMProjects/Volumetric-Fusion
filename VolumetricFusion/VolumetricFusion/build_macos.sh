cb47e2ccafe9944c8af040edd5d5941098071652
Brucknem <mbruckner94@gmail.com>
2019-10-22 15:43:00 +0200

    Removed all after gitignore change

--- VolumetricFusion/VolumetricFusion/build_macos.sh
+++
@@ -1,5 +0,0 @@
-# !/bin/zsh
-
-#rm -r CMakeCache.txt CMakeFiles/ ; cmake ../VolumetricFusion VolumetricFusion && make -j4 ../VolumetricFusion
-
-rm -r CMakeCache.txt CMakeFiles ; cmake . && make -j4
