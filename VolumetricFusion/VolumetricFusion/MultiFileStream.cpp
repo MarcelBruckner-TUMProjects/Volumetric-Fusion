3520daf388e827005bcf498bf5520fefed4ab491
Kevin Bein <mail@kevinbein.de>
2019-10-27 20:30:13 +0100

    Add pausing and exporting functionality

--- VolumetricFusion/VolumetricFusion/MultiFileStream.cpp
+++ VolumetricFusion/VolumetricFusion/MultiFileStream.cpp
@@ -17,9 +17,12 @@
 #include <string>
 #include <thread>
 #include <atomic>
+#include <filesystem>
 
 #include <imgui.h>
 #include "imgui_impl_glfw.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
 
 // Helper functions for rendering the UI
@@ -39,14 +42,14 @@ int main(int argc, char * argv[]) try {
     }
 
     // Create a simple OpenGL window for rendering:
-    window app(1280, 960, "VolumetricFusion - MultiStreamViewer");
+    window window_main(1280, 960, "VolumetricFusion - MultiStreamViewer");
 
-    ImGui_ImplGlfw_Init(app, false);
+    ImGui_ImplGlfw_Init(window_main, false);
 
     // Construct an object to manage view state
     glfw_state view_orientation{};
     // Let the user control and manipulate the scenery orientation
-    register_glfw_callbacks(app, view_orientation);
+    register_glfw_callbacks(window_main, view_orientation);
 
     view_orientation.yaw = -2.6;
     view_orientation.pitch = 0.8;
@@ -73,25 +76,35 @@ int main(int argc, char * argv[]) try {
     }
 
     // Declare filters
-    rs2::decimation_filter dec_filter(1);         // Decimation - reduces depth frame density
-    rs2::threshold_filter thr_filter(0.0f, 1.1f); // Threshold  - removes values outside recommended range
+    rs2::decimation_filter decimation_filter(1);         // Decimation - reduces depth frame density
+    rs2::threshold_filter threshold_filter(0.0f, 1.1f); // Threshold  - removes values outside recommended range
+    // 0 - fill_from_left - Use the value from the left neighbor pixel to fill the hole
+    // 1 - farest_from_around - Use the value from the neighboring pixel which is furthest away from the sensor
+    // 2 - nearest_from_around - - Use the value from the neighboring pixel closest to the sensor
+    //rs2::hole_filling_filter hole_filter(1); // terrible results ...
 
     std::vector<rs2::filter*> filters;
-    filters.emplace_back(&thr_filter);
-    filters.emplace_back(&dec_filter);
+    filters.emplace_back(&decimation_filter);
+    filters.emplace_back(&threshold_filter);
+    //filters.emplace_back(&hole_filter);
 
     // Create a thread for getting frames from the device and process them
     // to prevent UI thread from blocking due to long computations.
     std::atomic_bool stopped(false);
+    std::atomic_bool paused(false);
     std::vector<rs2::frame_queue> filtered_datas(4);
     std::vector<std::thread> processing_threads(4);
     rs2::frame_queue queue;
     for (int i = 0; i < 4; ++i) {
-        processing_threads[i] = std::thread([i, &stopped, &pipelines, &filters, &filtered_datas]() {
+        processing_threads[i] = std::thread([i, &stopped, &paused, &pipelines, &filters, &filtered_datas]() {
           auto pipe = pipelines[i].second;
           auto isi = pipelines[i].first == i;
           while (!stopped) //While application is running
           {
+              while (paused.load()) {
+                  continue;
+              }
+
               rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
               rs2::frame depth_frame = data.get_depth_frame(); //Take the depth frame from the frameset
               if (!depth_frame) { // Should not happen but if the pipeline is configured differently
@@ -111,44 +124,108 @@ int main(int argc, char * argv[]) try {
     }
 
     std::vector<rs2::pointcloud> filtered_pc(4);
-    std::vector<rs2::frame> filtered_colored(4);
+    std::vector<rs2::frame> active_frames(4);
     std::vector<rs2::points> filtered_points(4);
     std::vector<rs2::colorizer> color_maps(4);
 
-    while (app)
+    bool align_frames = false;
+
+    while (window_main)
     {
-        const float w = static_cast<float>(app.width());
-        const float h = static_cast<float>(app.height());
+        const float w = static_cast<float>(window_main.width());
+        const float h = static_cast<float>(window_main.height());
         const int w_half = w / 2;
         const int h_half = h / 2;
 
         //render_ui(w, h);
 
-        draw_text(10, 50, figure_filenames[0].c_str());
-        draw_text(w_half, 50, figure_filenames[1].c_str());
+        draw_text(10, 20, figure_filenames[0].c_str());
+        draw_text(w_half, 20, figure_filenames[1].c_str());
         draw_text(10, h_half + 10, figure_filenames[2].c_str());
         draw_text(w_half, h_half + 10, figure_filenames[3].c_str());
 
-        for (int i = 0; i < 4; ++i)
-        {
-            rs2::frame f;
-            if (filtered_datas[i].poll_for_frame(&f))  // Try to take the depth and points from the queue
-            {
-                filtered_points[i] = filtered_pc[i].calculate(f); // Generate pointcloud from the depth data
-                filtered_colored[i] = color_maps[i].process(f);     // Colorize the depth frame with a color map
-                filtered_pc[i].map_to(filtered_colored[i]);         // Map the colored depth to the point cloud
-                view_orientation.tex.upload(filtered_colored[i]);   //  and upload the texture to the view (without this the view will be B&W)
+        //if (ImGui::IsKeyReleased(ImGui::GetKeyIndex(ImGuiKey_C))) {
+        //}
+        // Flags for displaying ImGui window
+        static const int flags = ImGuiWindowFlags_NoCollapse
+                                 | ImGuiWindowFlags_NoScrollbar
+                                 | ImGuiWindowFlags_NoSavedSettings
+                                 | ImGuiWindowFlags_NoTitleBar
+                                 | ImGuiWindowFlags_NoResize
+                                 | ImGuiWindowFlags_NoMove;
 
-                if (filtered_colored[i] && filtered_points[i]) {
-                    glViewport(w_half * (i % 2), h_half - (h_half * (i / 2)), w_half, h_half);
-                    draw_pointcloud(int(w) / 2, int(h) / 2, view_orientation, filtered_points[i]);
-                }
-            } else {
+        ImGui_ImplGlfw_NewFrame(1);
+        ImGui::SetNextWindowSize({ w, h });
+        ImGui::Begin("window_main", nullptr, flags);
+        ImGui::SameLine(ImGui::GetWindowWidth() - 100);
+        auto button_save_frame_pressed = ImGui::Button("Save frames");
+        bool button_pause_pressed = false;
+        ImGui::SameLine(ImGui::GetWindowWidth() - 160);
+        if (!paused) {
+            button_pause_pressed = ImGui::Button("Pause");
+        } else {
+            button_pause_pressed = ImGui::Button("Resume");
+        }
+        ImGui::SameLine(ImGui::GetWindowWidth() - 260);
+        auto button_align_frames_pressed = ImGui::Button("Align frames");
+        ImGui::End();
+        ImGui::Render();
+
+        for (int i = 0; i < 4; ++i) {
+            rs2::frame f;
+            if (!paused && filtered_datas[i].poll_for_frame(&f)) { // Try to take the depth and points from the queue
+                filtered_points[i] = filtered_pc[i].calculate(f); // Generate pointcloud from the depth data
+                active_frames[i] = color_maps[i].process(f);     // Colorize the depth frame with a color map
+                filtered_pc[i].map_to(active_frames[i]);         // Map the colored depth to the point cloud
+            } else if (!paused) {
                 i -= 1; // avoids stuttering
+                continue;
+            }
+
+            if (active_frames[i] && filtered_points[i]) {
+                view_orientation.tex.upload(active_frames[i]);   //  and upload the texture to the view (without this the view will be B&W)
+                glViewport(w_half * (i % 2), h_half - (h_half * (i / 2)), w_half, h_half);
+                draw_pointcloud(int(w) / 2, int(h) / 2, view_orientation, filtered_points[i]);
             }
         }
+
+        if (button_save_frame_pressed) {
+            // Write images to disk
+            for (int i = 0; i < 4; ++i) {
+                auto vf = active_frames[i].as<rs2::video_frame>();
+
+                auto filename = figure_filenames[i];
+                filename = filename.erase(filename.find(".bag"), filename.length());
+
+                std::stringstream png_file;
+                png_file << "frame_" << filename << ".png";
+                stbi_write_png(png_file.str().c_str(), vf.get_width(), vf.get_height(),
+                               vf.get_bytes_per_pixel(), vf.get_data(), vf.get_stride_in_bytes());
+
+                std::string ply_file = "frame_" + filename + ".ply";
+                filtered_points[i].export_to_ply(ply_file, vf);
+
+                auto current_path = std::filesystem::current_path().u8string();
+                current_path = current_path.erase(current_path.length() - 1, 1);
+
+                std::cout << "Saved frame " << i << " to \"" << current_path << "/" << png_file.str() << "\""
+                          << std::endl;
+                std::cout << "Saved frame " << i << " to \"" << current_path << "/" << ply_file << "\""
+                          << std::endl;
+            }
+        }
+
+        if (button_pause_pressed) {
+            paused = !paused;
+        }
+
+        if (button_align_frames_pressed) {
+            std::cout << "Aligned the current lframes" << std::endl;
+        }
+
         // 15 frames per second are what I recorded the video at (avoids stuttering and reduces cpu load)
         nanosleep((const struct timespec[]){{0, 1000000000L / 15L}}, NULL);
+        //nanosleep((const struct timespec[]){{2, 0}}, NULL);
     }
 
     stopped = true;
