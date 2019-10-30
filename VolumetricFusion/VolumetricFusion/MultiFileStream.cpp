7b69ddaf92dbc591cf00fffc5fe8b69de30f3782
Brucknem <mbruckner94@gmail.com>
2019-10-30 22:41:28 +0100

    Major update for processing blocks Huge improvements for pointcloud generation Different rendering views

--- VolumetricFusion/VolumetricFusion/MultiFileStream.cpp
+++ VolumetricFusion/VolumetricFusion/MultiFileStream.cpp
@@ -31,9 +31,12 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+#include "ProcessingBlocks.hpp"
+
 enum class RenderState {
 	MULTI_POINTCLOUD,
-	SINGLE_COLOR,
+	ONLY_COLOR,
+	ONLY_DEPTH,
 	COUNT
 };
 
@@ -47,7 +50,7 @@ enum class CaptureState {
 class Settings {
 public:
 	CaptureState captureState = CaptureState::PLAYING;
-	RenderState renderState = RenderState::SINGLE_COLOR;
+	RenderState renderState = RenderState::ONLY_COLOR;
 	
 	std::string captures_folder = "captures/";
 
@@ -57,30 +60,31 @@ public:
 
 // Helper functions for rendering the UI
 void render_ui(float w, float h);
+rs2::processing_block createMedianBlurProcessingBlock();
 
-int main(int argc, char * argv[]) try {
+int main(int argc, char* argv[]) try {
 	//Settings settings;
-		
-    // Create a simple OpenGL window for rendering:
-    window window_main(1280, 960, "VolumetricFusion - MultiStreamViewer");
 
-    ImGui_ImplGlfw_Init(window_main, false);
+	// Create a simple OpenGL window for rendering:
+	window window_main(1280, 960, "VolumetricFusion - MultiStreamViewer");
 
-    // Construct an object to manage view state
-    glfw_state view_orientation{};
-    // Let the user control and manipulate the scenery orientation
-    register_glfw_callbacks(window_main, view_orientation);
+	ImGui_ImplGlfw_Init(window_main, false);
 
-    view_orientation.yaw = -2.6;
-    view_orientation.pitch = 0.8;
-    view_orientation.last_x = 738.7;
-    view_orientation.last_y = 1015.8;
-    view_orientation.offset_x = 2.0;
-    view_orientation.offset_y = -2.0;
+	// Construct an object to manage view state
+	glfw_state view_orientation{};
+	// Let the user control and manipulate the scenery orientation
+	register_glfw_callbacks(window_main, view_orientation);
 
-    rs2::context ctx; // Create librealsense context for managing devices
+	view_orientation.yaw = -2.6;
+	view_orientation.pitch = 0.8;
+	view_orientation.last_x = 738.7;
+	view_orientation.last_y = 1015.8;
+	view_orientation.offset_x = 2.0;
+	view_orientation.offset_y = -2.0;
+
+	rs2::context ctx; // Create librealsense context for managing devices
 	std::map<int, std::shared_ptr<rs2::pipeline>> pipelines;
-	
+
 	std::vector<std::string> stream_names(4);
 	int i = 0;
 	switch (settings.captureState) {
@@ -141,7 +145,7 @@ int main(int argc, char * argv[]) try {
 			stream_names[i] = device_name;
 		}
 	}
-		break;
+	break;
 
 	default:
 		break;
@@ -153,36 +157,64 @@ int main(int argc, char * argv[]) try {
 	while (stream_names.size() < 4) {
 		stream_names.push_back("");
 	}
-    
+
 
 	// We'll keep track of the last frame of each stream available to make the presentation persistent
 	std::map<int, rs2::frame> render_frames;
 
-	texture single_color_frame;
+	std::map<int, texture> textures;
+	std::map<int, rs2::colorizer> colorizers;
 
-    // Declare filters
-    rs2::decimation_filter decimation_filter(1);         // Decimation - reduces depth frame density
-    rs2::threshold_filter threshold_filter(0.0f, 1.1f); // Threshold  - removes values outside recommended range
-    // 0 - fill_from_left - Use the value from the left neighbor pixel to fill the hole
-    // 1 - farest_from_around - Use the value from the neighboring pixel which is furthest away from the sensor
-    // 2 - nearest_from_around - - Use the value from the neighboring pixel closest to the sensor
-    //rs2::hole_filling_filter hole_filter(1); // terrible results ...
+	// Declare filters
+	rs2::decimation_filter decimation_filter(1);         // Decimation - reduces depth frame density
+	rs2::threshold_filter threshold_filter(0.0f, 1.1f); // Threshold  - removes values outside recommended range
+	// 0 - fill_from_left - Use the value from the left neighbor pixel to fill the hole
+	// 1 - farest_from_around - Use the value from the neighboring pixel which is furthest away from the sensor
+	// 2 - nearest_from_around - - Use the value from the neighboring pixel closest to the sensor
+	//rs2::hole_filling_filter hole_filter(1); // terrible results ...
 
-    std::vector<rs2::filter*> filters;
-    filters.emplace_back(&decimation_filter);
-    filters.emplace_back(&threshold_filter);
-    //filters.emplace_back(&hole_filter);
+	std::vector<rs2::filter*> filters;
+	filters.emplace_back(&decimation_filter);
+	filters.emplace_back(&threshold_filter);
+	//filters.emplace_back(&hole_filter);
 
-    // Create a thread for getting frames from the device and process them
-    // to prevent UI thread from blocking due to long computations.
-    std::atomic_bool stopped(false);
-    std::atomic_bool paused(false);
-    std::vector<rs2::frame_queue> filtered_datas(4);
-    std::vector<rs2::frame> filtered_aligned_colors(4);
-    std::vector<std::thread> processing_threads(4);
-    rs2::frame_queue queue;
+	// Create a thread for getting frames from the device and process them
+	// to prevent UI thread from blocking due to long computations.
+	std::atomic_bool stopped(false);
+	std::atomic_bool paused(false);
+	std::atomic_bool depthProcessing(false);
+	std::atomic_bool colorProcessing(false);
+	//std::vector<rs2::frame_queue> filtered_datas(pipelines.size());
+	std::map<int, rs2::frame> filtered_color_frames;
+	std::map<int, rs2::frame> filtered_depth_frames;
+	std::map<int, std::thread> processing_threads;
+
+	std::map<int, rs2::pointcloud> filtered_pointclouds;
+	std::map<int, rs2::frame> colorized_depth_frames;
+	std::map<int, rs2::points> filtered_points;
+
+	// Create custom color processing block and their output queues:
+	std::map<int, rs2::frame_queue> color_processing_queues;
+	std::map<int, std::shared_ptr<rs2::processing_block>> color_processing_blocks;
+
+	// Create custom depth processing block and their output queues:
+	std::map<int, rs2::frame_queue> depth_processing_queues;
+	std::map<int, std::shared_ptr<rs2::processing_block>> depth_processing_blocks;
+
+	for (int i = 0; i < pipelines.size(); i++) {
+		color_processing_blocks[i] = std::make_shared<rs2::processing_block>(processing_blocks::createColorProcessingBlock([](cv::Mat& image) {
+			cv::medianBlur(image, image, 11);
+		}));
+		color_processing_blocks[i]->start(color_processing_queues[i]); // Bind output of the processing block to be enqueued into the queue
+
+		depth_processing_blocks[i] = std::make_shared<rs2::processing_block>(processing_blocks::createDepthProcessingBlock([](cv::Mat& image) {
+			cv::medianBlur(image, image, 11);
+		}));
+		depth_processing_blocks[i]->start(depth_processing_queues[i]); // Bind output of the processing block to be enqueued into the queue
+	}
+
 	for (int i = 0; i < pipelines.size(); ++i) {
-		processing_threads[i] = std::thread([i, &stopped, &paused, &pipelines, &filters, &filtered_datas, &filtered_aligned_colors]() {
+		processing_threads[i] = std::thread([&, i]() {
 			auto pipe = pipelines[i];
 
 			rs2::align align_to_color(RS2_STREAM_COLOR);
@@ -202,53 +234,66 @@ int main(int argc, char * argv[]) try {
 					if (!depth_frame) { // Should not happen but if the pipeline is configured differently
 						return;       //  it might not provide depth and we don't want to crash
 					}
-					rs2::frame filtered = depth_frame; // Does not copy the frame, only adds a reference
+
+					if (depthProcessing) {
+						// Send depth frame for processing
+						depth_processing_blocks[i]->invoke(depth_frame);
+						// Wait for results
+						depth_frame = depth_processing_queues[i].wait_for_frame();
+					}
+					rs2::frame filtered_depth_frame = depth_frame; // Does not copy the frame, only adds a reference
 
 					rs2::frame color_frame = data.get_color_frame();
-					filtered_aligned_colors[i] = color_frame;
+
+					if (colorProcessing) {
+						// Send color frame for processing
+						color_processing_blocks[i]->invoke(color_frame);
+						// Wait for results
+						color_frame = color_processing_queues[i].wait_for_frame();
+					}
+
+					filtered_color_frames[i] = color_frame;
 
 					// Apply filters.
 					for (auto&& filter : filters) {
-						filtered = filter->process(filtered);
+						filtered_depth_frame = filter->process(filtered_depth_frame);
 					}
 
 					// Push filtered & original data to their respective queues
-					filtered_datas[i].enqueue(filtered);
+					filtered_depth_frames[i] = filtered_depth_frame;
+
+					filtered_points[i] = filtered_pointclouds[i].calculate(depth_frame);  // Generate pointcloud from the depth data
+					colorized_depth_frames[i] = colorizers[i].process(depth_frame);		// Colorize the depth frame with a color map
+					filtered_pointclouds[i].map_to(colorized_depth_frames[i]);      // Map the colored depth to the point cloud
 				}
 				catch (const std::exception & e) {
 					std::stringstream stream;
-					stream << "******************** THREAD ERROR *******************" << std::endl << e.what() << "****************************************************" <<std::endl;
+					stream << "******************** THREAD ERROR *******************" << std::endl << e.what() << "****************************************************" << std::endl;
 				}
 			}
 			pipe->stop();
 		});
 	}
 
-
-    std::vector<rs2::pointcloud> filtered_pc(4);
-    std::vector<rs2::frame> active_frames(4);
-    std::vector<rs2::points> filtered_points(4);
-    std::vector<rs2::colorizer> color_maps(4);
-    texture color_image;
-
-    bool align_frames = false;
+	bool align_frames = false;
 
 	while (window_main)
 	{
-		const float w = static_cast<float>(window_main.width());
-		const float h = static_cast<float>(window_main.height());
-		const int w_half = w / 2;
-		const int h_half = h / 2;
+		int topOffset = 70;
+		const float width = static_cast<float>(window_main.width());
+		const float height = static_cast<float>(window_main.height()) - (topOffset * 0.5f);
+		const int width_half = width / 2;
+		const int height_half = height / 2;
 
 		// Retina display (Mac OS) have double the pixel density
 		int w2, h2;
 		glfwGetFramebufferSize(window_main, &w2, &h2);
-		const bool is_retina_display = w2 == w * 2 && h2 == h * 2;
+		const bool is_retina_display = w2 == width * 2 && h2 == height * 2;
 
 		draw_text(10, 20, stream_names[0].c_str());
-		draw_text(w_half, 20, stream_names[1].c_str());
-		draw_text(10, h_half + 10, stream_names[2].c_str());
-		draw_text(w_half, h_half + 10, stream_names[3].c_str());
+		draw_text(width_half, 20, stream_names[1].c_str());
+		draw_text(10, height_half + 10, stream_names[2].c_str());
+		draw_text(width_half, height_half + 10, stream_names[3].c_str());
 
 		// Flags for displaying ImGui window
 		static const int flags = ImGuiWindowFlags_NoCollapse
@@ -259,7 +304,7 @@ int main(int argc, char * argv[]) try {
 			| ImGuiWindowFlags_NoMove;
 		// UI Rendering
 		ImGui_ImplGlfw_NewFrame(1);
-		ImGui::SetNextWindowSize({ w, h });
+		ImGui::SetNextWindowSize({ width, height });
 		ImGui::Begin("window_main", nullptr, flags);
 
 		ImGui::SameLine(ImGui::GetWindowWidth() - 160);
@@ -275,8 +320,8 @@ int main(int argc, char * argv[]) try {
 		if (ImGui::Button("Save frames")) {
 			file_access::isDirectory(settings.captures_folder, true);
 			// Write images to disk
-			for (int i = 0; i < 4; ++i) {
-				auto vf = active_frames[i].as<rs2::video_frame>();
+			for (int i = 0; i < pipelines.size(); ++i) {
+				auto vf = colorized_depth_frames[i].as<rs2::video_frame>();
 
 				auto filename = std::to_string(vf.get_timestamp());
 				//filename = filename.erase(filename.find(".bag"), filename.length());
@@ -319,9 +364,9 @@ int main(int argc, char * argv[]) try {
 
 		ImGui::SameLine(ImGui::GetWindowWidth() - 360);
 		if (ImGui::Button("Switch view")) {
-			int s = (int) settings.renderState;
+			int s = (int)settings.renderState;
 			s = (s + 1) % (int)RenderState::COUNT;
-			settings.renderState = (RenderState) s;
+			settings.renderState = (RenderState)s;
 
 			std::cout << "Switching render state to " << std::to_string((int)settings.renderState) << std::endl;
 		}
@@ -337,69 +382,71 @@ int main(int argc, char * argv[]) try {
 			// Draw the pointclouds
 			for (int i = 0; i < pipelines.size() && i < 4; ++i)
 			{
-				rs2::frame f;
-				if (!paused && filtered_datas[i].poll_for_frame(&f)) { // Try to take the depth and points from the queue
-					filtered_points[i] = filtered_pc[i].calculate(f);  // Generate pointcloud from the depth data
-					active_frames[i] = color_maps[i].process(f);       // Colorize the depth frame with a color map
-					filtered_pc[i].map_to(active_frames[i]);           // Map the colored depth to the point cloud
-				}
-				else if (!paused) {
-					i -= 1; // avoids stuttering
-					continue;
-				}
-
-				if (active_frames[i] && filtered_points[i]) {
-					view_orientation.tex.upload(active_frames[i]);   //  and upload the texture to the view (without this the view will be B&W)
+				if (colorized_depth_frames[i] && filtered_points[i]) {
+					view_orientation.tex.upload(colorized_depth_frames[i]);   //  and upload the texture to the view (without this the view will be B&W)
 					if (is_retina_display) {
-						glViewport(w * (i % 2), h - (h * (i / 2)), w, h);
+						glViewport(width * (i % 2), height - (height * (i / 2)), width, height);
 					}
 					else {
-						glViewport(w_half * (i % 2), h_half - (h_half * (i / 2)), w_half, h_half);
+						glViewport(width_half * (i % 2), height_half - (height_half * (i / 2)), width_half, height_half);
 					}
 
-					if (filtered_aligned_colors[i]) {
-						draw_pointcloud_and_colors(w_half, h_half, view_orientation, filtered_points[i], filtered_aligned_colors[i], 0.2f);
+					if (filtered_color_frames[i]) {
+						draw_pointcloud_and_colors(width_half, height_half, view_orientation, filtered_points[i], filtered_color_frames[i], 0.2f);
 					}
 					else {
-						draw_pointcloud(w_half, h_half, view_orientation, filtered_points[i]);
+						draw_pointcloud(width_half, height_half, view_orientation, filtered_points[i]);
 					}
 				}
 			}
 		}
 		break;
 
-		case RenderState::SINGLE_COLOR:
+		case RenderState::ONLY_COLOR:
 		{
-			auto color = filtered_aligned_colors[0];
-			if (color != NULL) {
-				single_color_frame.render(color, { 0,0, window_main.width() , window_main.height() * 0.95f });
+			if (paused) {
+				break;
+			}
+			for (int i = 0; i < pipelines.size() && i < 4; ++i)
+			{
+				auto color = filtered_color_frames[0];
+				if (!paused && color != NULL) {
+					rect r{ width_half * (i % 2), height_half - (height_half * (i / 2)), width_half, height_half };
+					if (is_retina_display) {
+						r = rect{ width * (i % 2), height - (height * (i / 2)), width, height };
+					}
+					textures[i].render(color, r);
+				}
 			}
 
-			glBegin(GL_LINES);
+			break;
+		}
 
-			glColor3f(1, 0, 0);
+		case RenderState::ONLY_DEPTH:
+		{
+			for (int i = 0; i < pipelines.size() && i < 4; ++i)
+			{
+				rs2::frame depth = filtered_depth_frames[i];
+				if (!paused && depth != NULL) {
+					rect r{ width_half * (i % 2), height_half - (height_half * (i / 2)), width_half, height_half };
+					if (is_retina_display) {
+						r = rect{ width * (i % 2), height - (height * (i / 2)), width, height };
+					}
+					textures[i].render(colorizers[i].process(depth), r);
+				}
+			}
 
-			glVertex2d(50, 50);
-			glVertex2d(150, 50);
-			glVertex2d(150, 50);
-			glVertex2d(150, 150);
-			glVertex2d(150, 150);
-			glVertex2d(50, 150);
-			glVertex2d(50, 150);
-			glVertex2d(50, 50);
-
-			glEnd();
 			break;
 		}
 		}
 	}
 
-    stopped = true;
-    for (auto &thread : processing_threads) {
-        thread.join();
-    }
+	stopped = true;
+	for (auto& thread : processing_threads) {
+		thread.second.join();
+	}
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
 catch (const rs2::error & e)
 {
