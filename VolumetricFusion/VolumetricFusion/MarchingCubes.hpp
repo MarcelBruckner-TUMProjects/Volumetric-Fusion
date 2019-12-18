efd1ddd0a9117c9449b5eeda3fcf343201e6c8df
Brucknem <mbruckner94@gmail.com>
2019-12-18 14:33:00 +0100

    Added MarchingCubes file

---
+++ VolumetricFusion/VolumetricFusion/MarchingCubes.hpp
@@ -0,0 +1,16 @@
+#ifndef _MARCHING_CUBES_HEADER_
+#define _MARCHING_CUBES_HEADER_
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <VolumetricFusion\Voxelgrid.hpp>
+
+namespace vc::fusion {
+	void marchingCubes(vc::fusion::Voxelgrid* voxelgrid) {
+
+	}
+}
+
+#endif
